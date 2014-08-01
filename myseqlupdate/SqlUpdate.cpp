49886d7e12328f49cf7d238be930eab02dec4220
xiexhg <xiexhg@126.com>
2014-08-01 12:02:23 +0800

    add
    添加文件

---
+++ myseqlupdate/SqlUpdate.cpp
@@ -0,0 +1,12 @@
+#include "stdafx.h"
+#include "SqlUpdate.h"
+
+
+SqlUpdate::SqlUpdate(void)
+{
+}
+
+
+SqlUpdate::~SqlUpdate(void)
+{
+}
---
+++ myseqlupdate/SqlUpdate.h
@@ -0,0 +1,13 @@
+#pragma once
+#include "winsock2.h"
+class SqlUpdate
+{
+private:
+	int conn_flag;
+
+
+public:
+	SqlUpdate(void);
+	~SqlUpdate(void);
+};
+
